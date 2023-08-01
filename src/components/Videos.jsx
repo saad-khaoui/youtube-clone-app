d9f07d8266246375065364c997f2483011fd6f3d
saad kh <islamiccerv2015@gmail.com>
2023-07-31 20:08:34 -0700

    done

---
+++ src/components/Videos.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+        </Box>
+      ))}
+    </Stack>
+  );
+}
+
+export default Videos;
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+
+export {default as Navbar} from './Navbar.jsx'
+export {default as ChannelDetail} from './ChannelDetail.jsx'
+export {default as ChannelCard} from './ChannelCard.jsx'
+export {default as Feed} from './Feed.jsx'
+export {default as SearchFeed} from './SearchFeed.jsx'
+export {default as VideoDetail} from './VideoDetail.jsx'
+export {default as SearchBar} from './SearchBar.jsx'
+export {default as Sidebar } from './Sidebar';
+export {default as  Videos } from './Videos';
+export {default as VideoCard } from './VideoCard'
+/*export {default as Loader } from './Loader'*/
+
